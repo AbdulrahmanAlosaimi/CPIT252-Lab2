903a6ca9dc67000b6e1e7636be2994c45e3376af
AbdulrahmanAlosaimi <76831852+AbdulrahmanAlosaimi@users.noreply.github.com>
2022-02-06 11:42:45 +0300

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,11 @@
+
+public class Client {
+
+    public static void main(String[] args) {
+        Order o1 = new Order();
+        Shipment sh1 = new Shipment("Ahmed",
+                "248 NE. Pleasant St. Niceville, FL 32578",
+                "123-477-0001");
+        
+    }
+}
